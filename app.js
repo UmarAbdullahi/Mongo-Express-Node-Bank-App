8cba66b481c7a56168d161a7784dd4526b2f3be4
umarabdullahi <u.abdullahi001@gmail.com>
2020-04-11 12:28:50 +0100

    Read me

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ if (process.env.CURRENT_ENV === 'development') {
 // Start the app
 app.listen(CONFIG.port, async () => {
    console.log(
-      '%s App is running at http://localhost:%d in %s mode',
+      '%s App is running at http://localhost:%d in %s mode.',
       chalk.green('✓'),
       process.env.PORT,
       process.env.CURRENT_ENV
