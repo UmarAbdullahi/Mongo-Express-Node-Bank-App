d72623f72802713a5623368c070e8fc674f95db3
umarabdullahi <u.abdullahi001@gmail.com>
2020-04-11 12:17:55 +0100

    Unfinished Commit

---
+++ routes/users.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+// Controllers
+const usersCtrl = require('@controllers/users');
+
+// App
+const router = express.Router();
+
+// => GET /users/me
+// Get myself
+router.get('/me', usersCtrl.getMyself);
+
+router.get('/allusers', usersCtrl.getAllUsers);
+
+// => PUT /users/me
+// Update myself
+router.put('/me', usersCtrl.updateMyself);
+
+module.exports = router;
